ce4f500e5390406b15884ba7be36a6113bf0c253
syeda zaidi <zaidisyeda03@gmail.com>
2019-07-23 12:59:23 -0700

    added firebase config and linked to html

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+$(document).ready( function() {
+
+  // Your web app's Firebase configuration
+  var Config = {
+    apiKey: "AIzaSyDRpCTXsxWHA9pIpl-2RhRk8Kbn2AyAvIo",
+    authDomain: "train-scheduler-46710.firebaseapp.com",
+    databaseURL: "https://train-scheduler-46710.firebaseio.com",
+    projectId: "train-scheduler-46710",
+    storageBucket: "",
+    messagingSenderId: "1079248782101",
+    appId: "1:1079248782101:web:5cda48ff14f0014b",
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  
+
+
+
+});
