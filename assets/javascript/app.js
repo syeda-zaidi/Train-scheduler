72436f0da8ef3d1d8e5c8679a10b9d954be8001b
syeda zaidi <zaidisyeda03@gmail.com>
2019-07-29 13:37:46 -0700

    added momentjs and calc for train arrivals

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,32 +22,6 @@ $(document).ready(function(){
   var inputFirstTrain = "";
   var inputFrequency = "";
 
-  var newTrainData = [];
-
-  database.ref().on("child_added", function (snapshot){
-
-    console.log(snapshot.val());
-    console.log(snapshot.val().name);
-
-    var newTableRow = $("<tr>");
-    
-    var tableName = $("<td>").text(snapshot.val().name);
-    var tableDestination = $("<td>").text(snapshot.val().destination);
-    var tableFrequency = $("<td>").text(snapshot.val().frequency);
-    var FirstTrain = snapshot.val().firstTrain;
-    var tableNextArrival = $("<td>").text("next arrival");
-    var TableMinAway = $("<td>").text("min away");
-
-    newTableRow.append(tableName).append(tableDestination).append(tableFrequency).append(tableNextArrival).append(TableMinAway);
-  
-    $("#train-table").append(newTableRow);
-
-  }, function(errorObject) {
-
-    // In case of error this will print the error
-    console.log("The read failed: " + errorObject.code);
-
-  });
 
   $("#submit-btn").on("click", function (event) {
     event.preventDefault();
@@ -61,7 +35,6 @@ $(document).ready(function(){
     console.log(inputDestination);
     console.log(inputFirstTrain);
     console.log(inputFrequency);
-
     
     database.ref().push({
 
@@ -72,9 +45,60 @@ $(document).ready(function(){
 
     });
 
+    $("form").trigger("reset");
   });
 
 
+  database.ref().on("child_added", function (snapshot){
+
+    var FirstTrain = snapshot.val().firstTrain;
+    var tFreq = parseInt(snapshot.val().frequency);
+
+    // captures the time now
+    var now = moment();
+    console.log(now)
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var convertedFirstTrain = moment(FirstTrain, "HH:mm").subtract(1, "years");
+    console.log(convertedFirstTrain);
+    
+    
+    // calc diff of time in min btw now and first train 
+    var timeDiff = moment().diff(moment(convertedFirstTrain), 'minutes');
+    console.log(timeDiff);
+
+
+    var timeRemainder = timeDiff % tFreq;
+    console.log(timeRemainder);
+
+    var timeTillNextTrain = tFreq - timeRemainder;
+    console.log(timeTillNextTrain);
+    
+    var timeOfArrival = moment().add(timeTillNextTrain, 'minutes');
+    var nextTrain = moment(timeOfArrival).format("hh:mm");
+    console.log(nextTrain)
+
+
+    // creates new table row and add train data to page 
+    var newTableRow = $("<tr>");
+    
+    var tableName = $("<td>").text(snapshot.val().name);
+    var tableDestination = $("<td>").text(snapshot.val().destination);
+    var tableFrequency = $("<td>").text(snapshot.val().frequency);
+    var tableNextArrival = $("<td>").text(nextTrain);
+    var TableMinAway = $("<td>").text(timeTillNextTrain);
+
+    newTableRow.append(tableName).append(tableDestination).append(tableFrequency).append(tableNextArrival).append(TableMinAway);
+  
+    $("#train-table").append(newTableRow);
+
+
+  }, function(errorObject) {
+
+    // In case of error this will print the error
+    console.log("The read failed: " + errorObject.code);
+
+  });
 
 
 });
